bd5f73b34fa0556d5728c1712d3fabd038ac906b
lavanya-raheja <95681022+lavanya-raheja@users.noreply.github.com>
2024-06-29 00:09:45 +0530

    Create MaxSubarraySumKadane.java

---
+++ day-1/MaxSubarraySumKadane.java
@@ -0,0 +1,24 @@
+// Most Efficient Method: To find maximum subarray sum in a given array 
+
+public class MaxSubarraySumKadane {
+    public static void findMaxSubarraySum(int arr[]) { 
+        int maxSum = Integer.MIN_VALUE; 
+        int currSum = 0; 
+        for(int i = 0; i<arr.length; i++) {
+            if(currSum < 0) {
+                currSum = 0; 
+            }
+            currSum += arr[i]; 
+            if(currSum > maxSum) {
+                maxSum = currSum; 
+            }
+        }
+        System.out.println(maxSum);
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, -2, 6, -1, 3}; 
+        findMaxSubarraySum(arr); 
+    }
+}
+
+// EXPLANATION: Kadane's algorithm suggests that in order to find maximum sum in a subarray, we must discard negative results. So each time the currSum is negative, it becomes zero to ensure maximum sum is found. 
