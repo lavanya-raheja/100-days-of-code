3cc77662891363f51a1290c1d89c706ca0f3e010
lavanya-raheja <95681022+lavanya-raheja@users.noreply.github.com>
2024-07-06 00:30:24 +0530

    Create BubbleSort.java

---
+++ day-8/BubbleSort.java
@@ -0,0 +1,12 @@
+public static void bubbleSort(int arr[]) {
+        int n = arr.length-1; 
+        for(int i=0; i<n-1; i++) {
+            for(int j=0; j<n; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j]; 
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp; 
+                }
+            }
+        }
+}
